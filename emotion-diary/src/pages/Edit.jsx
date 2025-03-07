a3beb1abd911e2bb07ccb60dfc8f413a5bddf15e
김선희 <kimseonhee126@gimseonhuiui-MacBookAir.local>
2025-03-07 17:51:50 +0900

    feat: 다이어리 작성하기, 수정하기, 삭제하기, 뒤로 가기 구현 완료

--- emotion-diary/src/pages/Edit.jsx
+++ emotion-diary/src/pages/Edit.jsx
@@ -1,5 +1,8 @@
+import { useParams } from "react-router-dom";
+
 const Edit = () => {
-    return <div>Edit</div>
+    const params = useParams();
+    return <div>{params.id}번 일기 수정페이지입니다.</div>
 };
 
 export default Edit;
